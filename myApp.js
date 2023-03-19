7fdd7a3ffc132a44a7d2cbdbb9003c7576b4bedf
Beyza <melis4523>
2023-03-19 23:34:03 +0300

    challenge

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
 
@@ -12,17 +13,33 @@ let app = express();
 //#4
 app.use("/public", express.static(__dirname + "/public"))
 
-//#3
-// app.get("/", (req,res) => {
-//  res.sendFile(__dirname + '/views/index.html')
-// })
 
-//#5
-app.get("/json", (req,res) => {
- res.json({"message": "Hello json"})
+//#7
+app.use((req, res, next) => {
+ console.log(`${req.method} ${req.path} ${req.ip}`);
+ next()
 })
 
+//#3
+app.get("/", (req,res) => {
+ res.sendFile(__dirname + '/views/index.html')
+})
 
+//#5
+// app.get("/json", (req,res) => {
+//  res.json({"message": "Hello json"})
+// })
+
+//#6
+app.get("/json", (req,res) => {
+ if (process.env["MESSAGE_STYLE"] === "uppercase"){
+   res.json({"message": "HELLO JSON"})
+ }
+ else {
+  res.json({"message": "Hello json"})
+ }
+
+})
 
 
 
